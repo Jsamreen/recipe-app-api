f4f45c361fa19f603707f71fcebab92febb13a60
Javeria Samreen <javeria.samreen@cat.com>
2022-06-03 17:43:55 +0530

    db setup

--- app/app/calc.py
+++ app/app/calc.py
@@ -5,3 +5,7 @@ calculator functions
 def add(x,y):
     """add x and y"""
     return x + y
+
+def subtract(x,y):
+    """subtracting numbers"""
+    return y - x
--- app/app/tests.py
+++ app/app/tests.py
@@ -8,7 +8,7 @@ class CalcTests(SimpleTestCase):
     """test calc module"""
 
     def test_add_numbers(self):
-        
+
         res = calc.add(5,6)
         
         self.assertEqual(res,11)
