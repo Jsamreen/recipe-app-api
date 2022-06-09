af4268a1c153d12c42dd737d953d00c76466b276
Javeria Samreen <javeria.samreen@cat.com>
2022-06-09 18:43:11 +0530

    adding wait for db and migrations command.

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,10 +2,12 @@
 calculator functions
 """
 
-def add(x,y):
+
+def add(x, y):
     """add x and y"""
     return x + y
 
-def subtract(x,y):
+
+def subtract(x, y):
     """subtracting numbers"""
     return y - x
